401d19ffd69acf1efe3256700c655a0c8bbd15db
Neyokee <geka2678@gmail.com>
2022-04-10 00:36:54 +0300

    new design1

--- Productlist/settings.py
+++ Productlist/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # AUTH_USER_MODEL = 'List.CustomUser'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
